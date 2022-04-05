8e2817dcaa8c14b8cd722efe0af091adbdf2d483
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2022-04-05 19:02:23 +1200

    Adding pytest

---
+++ test/test_pytest.py
@@ -0,0 +1,9 @@
+# import sys
+# import os.path
+# sys.path.append(
+#     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+# from ngoto.util import Logging
+
+def test_pytest():
+    return True
