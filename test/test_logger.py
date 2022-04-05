5aac48cdc6baf9668a23c5c48354a50f9b622c55
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2022-04-05 18:38:36 +1200

    Fix Logging Tests
    Remove the calling of function setLevel - function has been removed

--- test/test_logger.py
+++ test/test_logger.py
@@ -8,13 +8,11 @@ from ngoto.util import Logging
 def test_logger_add():
     """ Test logger by adding debug message and checking in logs"""
     logger: Logging = Logging()
-    logger.setLevel('CRITICAL')
     logger.debug('debug')
     assert(logger.get_log() == '\n\x1b[0m\x1b[94m[DEBUG]debug\x1b[0m\n')
 
 def test_logger_level():
     """ Checks logger if on lower level message is not in log"""
     logger: Logging = Logging()
-    logger.setLevel('DEBUG')
     logger.error('info')
     assert(logger.get_log().strip() == '\x1b[0m')
